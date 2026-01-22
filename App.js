5bd08a70f52bd304cd6aa8fe2cabe1b52440692b
Adarsh Kumar <rajpandeyji108@gmail.com>
2026-01-22 20:52:35 +0530

    used parcel and install react and react-dom

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
 const heading = React.createElement('div', {}, [React.createElement('p', {}, "Hello World from React CDN"), React.createElement('h3', {}, "Nested element using react cdn")]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # i am learing react
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
